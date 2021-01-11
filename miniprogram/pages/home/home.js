b43a16ebf758ee2d839eab8f546fe03caefb6bc2
Autumnal_Joy <autumnal_joy@foxmail.com>
2021-01-11 15:19:55 +0800

    home/locate
    1. home页面增加120拨号, 模拟器无法跳转, 可能真机调试会有效果
    2. locate图标绑定定位函数

--- miniprogram/pages/home/home.js
+++ miniprogram/pages/home/home.js
@@ -1,80 +1,60 @@
 // pages/home/home.js
 Page({
-
   /**
    * 页面的初始数据
    */
-  data: {
+  data: {},
 
-  },
-
-  onGetLoc(){
-    wx.getLocation({
-      type: 'gcj02', //返回可以用于wx.openLocation的经纬度
-      success (res) {
-        const latitude = res.latitude
-        const longitude = res.longitude
-        wx.openLocation({
-          latitude,
-          longitude,
-        })
-      }
-     })
+  callHospital() {
+    wx.makePhoneCall({
+      phoneNumber: 120,
+      success: (result) => {
+        console.log("success");
+      },
+      fail: () => {
+        console.log("fail(可能需要真机调试?)");
+      },
+      complete: () => {},
+    });
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
-
-  },
+  onLoad: function (options) {},
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
-  onReady: function () {
-
-  },
+  onReady: function () {},
 
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow: function () {
-
-  },
+  onShow: function () {},
 
   /**
    * 生命周期函数--监听页面隐藏
    */
-  onHide: function () {
-
-  },
+  onHide: function () {},
 
   /**
    * 生命周期函数--监听页面卸载
    */
-  onUnload: function () {
-
-  },
+  onUnload: function () {},
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
-  onPullDownRefresh: function () {
-
-  },
+  onPullDownRefresh: function () {},
 
   /**
    * 页面上拉触底事件的处理函数
    */
-  onReachBottom: function () {
-
-  },
+  onReachBottom: function () {},
 
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage: function () {
-
-  }
-})
+  onShareAppMessage: function () {},
+});
