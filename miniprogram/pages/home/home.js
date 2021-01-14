84a8688f2a302bff144eb3f55ce18bec7fe6a702
Autumnal_Joy <autumnal_joy@foxmail.com>
2021-01-14 16:00:27 +0800

    fix: home页面联系方式正确显示

--- miniprogram/pages/home/home.js
+++ miniprogram/pages/home/home.js
@@ -7,20 +7,9 @@ Page({
    */
   data: {
     index: 0,
-    phones: [
-      {
-        key: 0,
-        number: 120,
-      },
-      {
-        key: 1,
-        number: 110,
-      },
-      {
-        key: 2,
-        number: 10086,
-      },
-    ],
+    person_info: {
+      emergencyCall: [],
+    },
   },
 
   onPickerChange(event) {
@@ -31,7 +20,7 @@ Page({
 
   makePhoneCall() {
     let phoneNumber = this.data.phones[this.data.index].number;
-    apiTools.call(phoneNumber.toString()).catch(err => {
+    apiTools.call(phoneNumber).catch(err => {
       console.log(err);
     });
   },
@@ -55,7 +44,53 @@ Page({
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow: function () {},
+  onShow: async function () {
+    console.group("home.onShow");
+    try {
+      var { data: person_info } = await wx.getStorage({
+        key: "person_info",
+      });
+      console.log("读取本地缓存", person_info);
+    } catch (err) {
+      console.log("本地缓存读取失败", err);
+      try {
+        let _openid = await app.globalData.login();
+        let collection = wx.cloud
+          .database({
+            env: "chuyan-5g4flozv2fa0a4f5",
+          })
+          .collection("person_info");
+        let list = await collection
+          .where({
+            _openid,
+          })
+          .get();
+        console.log("读取云数据库", list);
+        if (list.data.length) {
+          person_info = list.data[0];
+        } else {
+          person_info = {};
+        }
+      } catch (err) {
+        console.log("云数据库读取失败", err);
+      } finally {
+        try {
+          let res = await wx.setStorage({
+            key: "person_info",
+            data: person_info,
+          });
+          console.log("设置本地缓存", res);
+        } catch (err) {
+          console.log("本地缓存设置失败", err);
+        }
+      }
+    } finally {
+      this.setData({
+        person_info: { ...this.data.person_info, ...person_info },
+      });
+    }
+    console.groupEnd();
+  },
 
   /**
    * 生命周期函数--监听页面隐藏
