1e7d81e1df3a6119f40772f86def54466cb01e21
Autumnal_Joy <autumnal_joy@foxmail.com>
2021-01-15 21:47:16 +0800

    合并route子页面

--- miniprogram/pages/route/transport/transport.js
+++ miniprogram/pages/route/transport/transport.js
@@ -1,71 +1,57 @@
-// miniprogram/pages/route/walk/walk.js
+// pages/route/transport/transport.js
 Page({
-
   /**
    * 页面的初始数据
    */
   data: {
-
+    src: "",
   },
-  go2: function(options){
-    wx.switchTab({
-       url: '/pages/route/route',
-       })
+  go2: function (options) {
+    wx.wx.navigateBack({
+      delta: 1,
+    });
   },
-
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    this.setData({
+      src: "/pages/route/icons/" + options.type + ".jpg",
+    });
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
-  onReady: function () {
-
-  },
+  onReady: function () {},
 
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow: function () {
-
-  },
+  onShow: function () {},
 
   /**
    * 生命周期函数--监听页面隐藏
    */
-  onHide: function () {
-
-  },
+  onHide: function () {},
 
   /**
    * 生命周期函数--监听页面卸载
    */
-  onUnload: function () {
-
-  },
+  onUnload: function () {},
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
-  onPullDownRefresh: function () {
-
-  },
+  onPullDownRefresh: function () {},
 
   /**
    * 页面上拉触底事件的处理函数
    */
-  onReachBottom: function () {
-
-  },
+  onReachBottom: function () {},
 
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage: function () {
-
-  }
-})
+  onShareAppMessage: function () {},
+});
